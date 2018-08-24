e8d198b8c7a33a07afac03b15c9605be5b97f5c2
Christopher Weber <chrisrrweber@gmail.com>
2018-08-24 16:21:35 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -21,7 +21,7 @@ contacts.each do |person, data|
 
       if attribute == :favorite_icecream_flavors
       value.each do |flavor|
-      flavor.delete.if("strawberry")
+      flavor.delete.if(["strawberry"])
       end
     end
   end
